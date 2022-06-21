48263b73a5c15dbc4baf9ccba76a06ab57d14aa4
isha614 <ishamehta614@gmail.com>
2022-06-21 11:22:20 +0530

    structure designed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./Components/NavBar";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Services from "./Components/Services";
+import TeamMembers from "./Components/TeamMembers";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App=()=>{
+  return(
+    <>
+         <NavBar/>
+         <Home/>
+         <About/>
+         <Services/>
+         <TeamMembers/>
+         <Contact/>
+         <Footer/>
+    </>
+  )
 }
-
 export default App;
---
+++ src/Components/TeamMembers.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TeamMembers=()=>{
+    return (
+        <>
+        <h1>
+            TeamMembers
+        </h1>
+        </>
+    )
+}
+export default TeamMembers;
