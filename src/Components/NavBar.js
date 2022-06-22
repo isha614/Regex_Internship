c67df695fd85a0793a6fb01b12a843e8009b9331
isha614 <ishamehta614@gmail.com>
2022-06-22 22:41:07 +0530

    footer and changes done

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar=()=>{
         <div class="container-xxl position-relative p-0">
             <nav class="navbar navbar-expand-lg navbar-light  px-4 px-lg-5 py-3 py-lg-0">
                 <a href="index.html" class="navbar-brand p-0">
-                    <h1 class="m-0">Regex</h1>
+                    <h1 class="m-0">RegEx</h1>
                    
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
