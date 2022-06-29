13eb7828a81362fba1b34fe9e48b744983d2c649
isha614 <ishamehta614@gmail.com>
2022-06-29 20:00:52 +0530

    structure updated

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./Footer.css";
 const Footer=()=>{
     return (
         <div className="container-fluid bg-dark text-light footer pt-5 wow fadeIn" data-wow-delay="0.1s" style={{marginTop: '6rem'}}>
