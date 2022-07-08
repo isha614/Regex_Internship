d6527d2bdf51416b759f3c1aeb4bcb1efc7cd90d
Darshan <mr.darshanghorpade@gmail.com>
2022-07-08 11:18:20 +0530

    Solving the errors

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,63 +1,103 @@
 import React from "react";
+
 import "./Testimonial.css";
 
 export default function Testimonial() {
   return (
     <>
+      <div class="container-xxl bg-white p-0">
         <div className="container-xxl py-6">
-            <div className="container">
-                <div className="mx-auto text-center wow fadeInUp" data-wow-delay="0.1s" style={{maxWidth: 600}}>
-                    <div className="d-inline-block border rounded-pill text-primary px-4 mb-3">Testimonial</div>
-                    <h2 className="mb-5">What Our Clients Say!</h2>
-                </div>
-                <div className="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
-                    <div className="testimonial-item rounded p-4">
-                        <i className="fa fa-quote-left fa-2x text-primary mb-3"></i>
-                        <p>Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
-                        <div className="d-flex align-items-center">
-                            <img className="img-fluid flex-shrink-0 rounded-circle" src="Image/testimonial-1.jpg"/>
-                            <div className="ps-3">
-                                <h6 className="mb-1">Client Name</h6>
-                                <small>Profession</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="testimonial-item rounded p-4">
-                        <i className="fa fa-quote-left fa-2x text-primary mb-3"></i>
-                        <p>Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
-                        <div className="d-flex align-items-center">
-                            <img className="img-fluid flex-shrink-0 rounded-circle" src="Image/testimonial-2.jpg"/>
-                            <div className="ps-3">
-                                <h6 className="mb-1">Client Name</h6>
-                                <small>Profession</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="testimonial-item rounded p-4">
-                        <i className="fa fa-quote-left fa-2x text-primary mb-3"></i>
-                        <p>Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
-                        <div className="d-flex align-items-center">
-                            <img className="img-fluid flex-shrink-0 rounded-circle" src="Image/testimonial-3.jpg"/>
-                            <div className="ps-3">
-                                <h6 className="mb-1">Client Name</h6>
-                                <small>Profession</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="testimonial-item rounded p-4">
-                        <i className="fa fa-quote-left fa-2x text-primary mb-3"></i>
-                        <p>Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
-                        <div className="d-flex align-items-center">
-                            <img className="img-fluid flex-shrink-0 rounded-circle" src="Image/testimonial-4.jpg"/>
-                            <div className="ps-3">
-                                <h6 className="mb-1">Client Name</h6>
-                                <small>Profession</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+          <div className="container">
+            <div
+              className="mx-auto text-center wow fadeInUp"
+              data-wow-delay="0.1s"
+              style={{ maxWidth: 600 }}
+            >
+              <div className="d-inline-block border rounded-pill text-primary px-4 mb-3">
+                Testimonial
+              </div>
+              <h2 className="mb-5">What Our Clients Say!</h2>
             </div>
+            <div
+              className="owl-carousel testimonial-carousel wow fadeInUp"
+              data-wow-delay="0.1s"
+            >
+              <div className="testimonial-item rounded p-4">
+                <i className="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                <p>
+                  Dolor et eos labore, stet justo sed est sed. Diam sed sed
+                  dolor stet amet eirmod eos labore diam
+                </p>
+                <div className="d-flex align-items-center">
+                  <img
+                    className="img-fluid flex-shrink-0 rounded-circle"
+                    src="Image/testimonial-1.jpg"
+                    alt="testimonial-1"
+                  />
+                  <div className="ps-3">
+                    <h6 className="mb-1">Client Name</h6>
+                    <small>Profession</small>
+                  </div>
+                </div>
+              </div>
+              <div className="testimonial-item rounded p-4">
+                <i className="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                <p>
+                  Dolor et eos labore, stet justo sed est sed. Diam sed sed
+                  dolor stet amet eirmod eos labore diam
+                </p>
+                <div className="d-flex align-items-center">
+                  <img
+                    className="img-fluid flex-shrink-0 rounded-circle"
+                    src="Image/testimonial-2.jpg"
+                    alt="testimonial-2"
+                  />
+                  <div className="ps-3">
+                    <h6 className="mb-1">Client Name</h6>
+                    <small>Profession</small>
+                  </div>
+                </div>
+              </div>
+              <div className="testimonial-item rounded p-4">
+                <i className="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                <p>
+                  Dolor et eos labore, stet justo sed est sed. Diam sed sed
+                  dolor stet amet eirmod eos labore diam
+                </p>
+                <div className="d-flex align-items-center">
+                  <img
+                    className="img-fluid flex-shrink-0 rounded-circle"
+                    src="Image/testimonial-3.jpg"
+                    alt="testimonial-3"
+                  />
+                  <div className="ps-3">
+                    <h6 className="mb-1">Client Name</h6>
+                    <small>Profession</small>
+                  </div>
+                </div>
+              </div>
+              <div className="testimonial-item rounded p-4">
+                <i className="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                <p>
+                  Dolor et eos labore, stet justo sed est sed. Diam sed sed
+                  dolor stet amet eirmod eos labore diam
+                </p>
+                <div className="d-flex align-items-center">
+                  <img
+                    className="img-fluid flex-shrink-0 rounded-circle"
+                    src="Image/testimonial-4.jpg"
+                    alt="testimonial-4"
+                  />
+                  <div className="ps-3">
+                    <h6 className="mb-1">Client Name</h6>
+                    <small>Profession</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import {createRoot} from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-
-
-
-ReactDOM.render(
+root.render(
   <BrowserRouter>
-
     <App />
-
-
-  </BrowserRouter>,
-
-  document.getElementById('root')
-
+  </BrowserRouter>
 );
-
