f37b24f18d5e0fe9bb1d454ba9ea1c3618eac16c
Isha Mehta <71143297+isha614@users.noreply.github.com>
2022-07-10 19:12:34 +0530

    icon added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "../../bootstrap.min.css";
 import "./Footer.css";
+import BackToTop from "./BackToTop";
 const Footer=()=>{
     return (
+      <>
         <div className="container-fluid bg-dark text-light footer pt-5 wow fadeIn" data-wow-delay="0.1s" style={{marginTop: '6rem'}}>
         <div className="container py-5">
           <div className="row g-5">
@@ -62,12 +64,14 @@ const Footer=()=>{
                
               </div>
               <div className="col-md-6 text-center text-md-end">
-                
+              <a href="#" classname="btn btn-lg btn-primary btn-lg-square back-to-top"><i classname="bi bi-arrow-up" /></a>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <BackToTop/>
+      </>
     )
 }
 export default Footer;
