9861cec236b31deb6977af104038e8ae898baa83
Isha Mehta <71143297+isha614@users.noreply.github.com>
2022-07-13 20:07:09 +0530

    contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,9 +3,50 @@ import "./Contact.css";
 const Contact=()=>{
     return (
         <>
-        <h1>
-            Contact Page
-        </h1>
+         <div className="container-xxl py-6">
+        <div className="container">
+          <div className="mx-auto text-center wow fadeInUp" data-wow-delay="0.1s" style={{maxWidth: '600px'}}>
+            <div className="d-inline-block border rounded-pill text-primary px-4 mb-3">Contact Us</div>
+            <h2 className="mb-5">If You Have Any Query, Please Feel Free Contact Us</h2>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-lg-7 wow fadeInUp" data-wow-delay="0.3s">
+              <p className="text-center mb-4">The contact form is currently inactive. Get a functional and working contact form with Ajax &amp; PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
+              <form>
+                <div className="row g-3">
+                  <div className="col-md-6">
+                    <div className="form-floating">
+                      <input type="text" className="form-control" id="name" placeholder="Your Name" />
+                      <label htmlFor="name">Your Name</label>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-floating">
+                      <input type="email" className="form-control" id="email" placeholder="Your Email" />
+                      <label htmlFor="email">Your Email</label>
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <div className="form-floating">
+                      <input type="text" className="form-control" id="subject" placeholder="Subject" />
+                      <label htmlFor="subject">Subject</label>
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <div className="form-floating">
+                      <textarea className="form-control" placeholder="Leave a message here" id="message" style={{height: '150px'}} defaultValue={""} />
+                      <label htmlFor="message">Message</label>
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <button className="btn btn-primary w-100 py-3" type="submit">Send Message</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
         </>
     )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,94 +2,16 @@ import React from "react";
 import NavBar from "../components/NavBar/NavBar";
 import Banner from "../components/Banner/Banner";
 import Footer from "../components/Footer/Footer";
+import Contact from "../components/Contact/Contact";
 
-const Contact = () => {
+const ContactApp = () => {
   return (
     <>
       <NavBar />
       <Banner title="Contact Us" submenu="Contact" />
-      {/* Contact Start */}
-      <div className="container">
-        <div
-          className="mx-auto text-center wow fadeInUp"
-          data-wow-delay="0.1s"
-          style={{ maxWidth: "600px" }}
-        >
-          <div className="d-inline-block border rounded-pill text-primary px-4 mb-3">
-            Contact Us
-          </div>
-          <h2 className="mb-5">
-            If You Have Any Query, Please Feel Free Contact Us
-          </h2>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-lg-7 wow fadeInUp" data-wow-delay="0.3s">
-            <p className="text-center mb-4">
-              The contact form is currently inactive. Get a functional and
-              working contact form with Ajax &amp; PHP in a few minutes. Just
-              copy and paste the files, add a little code and you're done.{" "}
-              <a href="https://htmlcodex.com/contact-form">Download Now</a>.
-            </p>
-            <form>
-              <div className="row g-3">
-                <div className="col-md-6">
-                  <div className="form-floating">
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="name"
-                      placeholder="Your Name"
-                    />
-                    <label htmlFor="name">Your Name</label>
-                  </div>
-                </div>
-                <div className="col-md-6">
-                  <div className="form-floating">
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="email"
-                      placeholder="Your Email"
-                    />
-                    <label htmlFor="email">Your Email</label>
-                  </div>
-                </div>
-                <div className="col-12">
-                  <div className="form-floating">
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="subject"
-                      placeholder="Subject"
-                    />
-                    <label htmlFor="subject">Subject</label>
-                  </div>
-                </div>
-                <div className="col-12">
-                  <div className="form-floating">
-                    <textarea
-                      className="form-control"
-                      placeholder="Leave a message here"
-                      id="message"
-                      style={{ height: "150px" }}
-                      defaultValue={""}
-                    />
-                    <label htmlFor="message">Message</label>
-                  </div>
-                </div>
-                <div className="col-12">
-                  <button className="btn btn-primary w-100 py-3" type="submit">
-                    Send Message
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-      {/* Contact End */}
+      <Contact/> 
       <Footer />
     </>
   );
 };
-export default Contact;
+export default ContactApp;
